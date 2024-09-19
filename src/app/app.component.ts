f693996cd57e1c7f732940912378b67e105ec607
imilen <m.ivanov@email.com>
2024-09-19 22:47:07 +0300

    feat: add examples how to use directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CardComponent } from './components/card/card.component';
+import { TemplateDirective } from './directives/template/template.directive';
+import { Card } from './models/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, CardComponent, TemplateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-typed-template';
+
+  cardType!: Card;
+
+  cards: Card[] = Array(4).fill(0).map((_v, i) => ({ id: (i + 1) * 1000, number: 'xxxxxxxx' }));
+
+  constructor() { }
+
+  $as<T>(data: unknown, _model: T | undefined): T {
+    return data as unknown as T;
+  }
 }
